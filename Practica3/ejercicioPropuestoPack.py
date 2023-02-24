c4a812879ab6ed6d2079b6c2fb9fcc208f6132e8
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-02-24 11:42:43 +0100

    Mejora código y documentación ejer sesion 3
    Uso de try catch, para control de excepciones
    Uso de with, para el cierre automático de ficheros

--- Practica3/ejercicioPropuestoPack.py
+++ Practica3/ejercicioPropuestoPack.py
@@ -1,33 +1,35 @@
 import struct
 
-# Fichero que contiene las llamadas (1 por línea) en formato
-# hora,minuto,telefono1,telefono2,duracion
-f = open('Llamadas.txt', 'r')
+try:
+    # i: Fichero que contiene las llamadas (1 por línea) en formato
+    # hora,minuto,telefono1,telefono2,duracion
 
-# Fichero donde pondremos los datos empaquetados 
-o = open('dummy~', 'wb')
+    # o: Fichero donde pondremos los datos empaquetados 
+    with open('data/Llamadas.txt', 'r') as i, open('data/dummy~', 'wb') as o:
 
-# Leemos cada línea
-for line in f:
-    # Separamos la linea por las ','
-    data = line.split(',')
-    
-    # se pasa a entero (porque estaba en String) y ese se convierte a char (para forzar que sea 1 Byte), luego, se codifica
-    hour = chr(int(data[0])).encode()
-    minute = chr(int(data[1])).encode()
+        # Leemos cada línea
+        for line in i:
+            # Separamos la linea por las ','
+            data = line.split(',')
+            
+            # se pasa a entero (porque estaba en String) y ese se convierte a char (para forzar que sea 1 Byte), luego, se codifica
+            hour = chr(int(data[0])).encode()
+            minute = chr(int(data[1])).encode()
 
-    # sólo se pasa a entero, suponemos que tienen el tamaño esperado 4B y 2B
-    callerPhone = int(data[2])
-    reciverPhone = int(data[3])
-    duration = int(data[4])
+            # sólo se pasa a entero, suponemos que tienen el tamaño esperado 4B y 2B
+            callerPhone = int(data[2])
+            reciverPhone = int(data[3])
+            duration = int(data[4])
 
-    # empaquetamos los datos en el orden esperado
-    packedData = struct.pack('!cciih', hour, minute, callerPhone, reciverPhone, duration)
+            # empaquetamos los datos en el orden esperado
+            packedData = struct.pack('!cciih', hour, minute, callerPhone, reciverPhone, duration)
 
-    # Escribimos los datos empaquetados en el archivo
-    o.write(packedData)
+            # Escribimos los datos empaquetados en el archivo
+            o.write(packedData)
+            print(f"Succesfully processed line: {line}", end = "")
 
-# cerramos los ficheros
-f.close()
-o.close()
+except FileNotFoundError as fnfe:
+    print("The file does not exit")
+except KeyboardInterrupt as ki:
+    print("The process has been killed")
 
--- Practica3/ejercicioPropuestoUnpack.py
+++ Practica3/ejercicioPropuestoUnpack.py
@@ -1,34 +1,38 @@
 import struct
 
-# open a file in read bits mode
-f = open('ListinLlamadas', 'rb')
+try:
+    # open a file in read binary mode
+    with open("data/ListinLlamadas", "rb") as f:
 
-# se intentan leer datos, 12 bytes ya que es lo que ocupa cada registo, este tamaño viene dado por 'cciih'
-packedData = f.read(12)
+        # se intentan leer datos, 12 bytes ya que es lo que ocupa cada registo, este tamaño viene dado por 'cciih'
+        packedData = f.read(12)
 
-# Mientras nos queden datos por leer (y estos ocupen lo que queremos)
-while len(packedData) == 12:
+        # Mientras nos queden datos por leer (y estos ocupen lo que queremos)
+        while len(packedData) == 12:
 
-    # desempaquetamos los datos en 2 chars, 2 int y 1 short
-    UnpackedData = struct.unpack('!cciih', packedData)
+            # desempaquetamos los datos en 2 chars, 2 int y 1 short
+            UnpackedData = struct.unpack("!cciih", packedData)
 
-    # pasamos a Byte (dato numérico) los chars
-    hour = ord(UnpackedData[0])
-    minute = ord(UnpackedData[1])
+            # pasamos a Byte (dato numérico) los chars
+            hour = ord(UnpackedData[0])
+            minute = ord(UnpackedData[1])
 
-    # 2 int 4B
-    callerPhone = UnpackedData[2]
-    reciverPhone = UnpackedData[3]
+            # 2 int 4B
+            callerPhone = UnpackedData[2]
+            reciverPhone = UnpackedData[3]
 
-    # short 2B
-    duration = UnpackedData[4]
+            # short 2B
+            duration = UnpackedData[4]
 
-    # mostramos la información leída
-    print('The call made from {0} to {1} started at {2:2d}:{3:02d} and lasted {4} minutes'.format(callerPhone, reciverPhone, hour, minute, duration))
+            # mostramos la información leída
+            print("The call made from {0} to {1} started at {2:2d}:{3:02d} and lasted {4} minutes".format(callerPhone, reciverPhone, hour, minute, duration))
+            print(f"{hour},{minute},{callerPhone},{reciverPhone},{duration}")
 
-    # intentamos leer los siguientes datos
-    datos = f.read(12)
+            # intentamos leer los siguientes datos
+            packedData = f.read(12)
 
-# cerramos el archivo
-f.close()
+except FileNotFoundError as fnfe:
+    print("The file does not exit")
+except KeyboardInterrupt as ki:
+    print("The process has been killed")
 
