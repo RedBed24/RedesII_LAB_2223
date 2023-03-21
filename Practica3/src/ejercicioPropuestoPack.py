1607c72df078c8eb1a3cbdaf9c683a096a5b17c7
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-03-21 19:19:16 +0100

    Uso de try catch en la práctica 3

--- Practica3/src/ejercicioPropuestoPack.py
+++ Practica3/src/ejercicioPropuestoPack.py
@@ -1,35 +1,40 @@
 import struct
 
+PATH_TO_INPUT_FILE = "data/Llamadas.txt"
+PATH_TO_OUTPUT_FILE = "data/output~"
+
 try:
-    # i: Fichero que contiene las llamadas (1 por línea) en formato
-    # hora,minuto,telefono1,telefono2,duracion
+	# inputFile: Fichero que contiene las llamadas (1 por línea) en formato
+	# hora,minuto,teléfono1,teléfono2,duración
 
-    # o: Fichero donde pondremos los datos empaquetados 
-    with open('data/Llamadas.txt', 'r') as i, open('data/dummy~', 'wb') as o:
+	# outputFile: Fichero donde pondremos los datos empaquetados
+	with open(PATH_TO_INPUT_FILE, "r") as inputFile, open(PATH_TO_OUTPUT_FILE, "wb") as outputFile:
 
-        # Leemos cada línea
-        for line in i:
-            # Separamos la linea por las ','
-            data = line.split(',')
-            
-            # se pasa a entero (porque estaba en String) y ese se convierte a char (para forzar que sea 1 Byte), luego, se codifica
-            hour = chr(int(data[0])).encode()
-            minute = chr(int(data[1])).encode()
+		# Leemos cada línea
+		for line in inputFile:
+			# Separamos la linea por las ","
+			data = line.split(",")
+			
+			# se pasa a entero (porque estaba en String) y ese se convierte a char (para forzar que sea 1 Byte), luego, se codifica
+			hour = chr(int(data[0])).encode()
+			minute = chr(int(data[1])).encode()
 
-            # sólo se pasa a entero, suponemos que tienen el tamaño esperado 4B y 2B
-            callerPhone = int(data[2])
-            reciverPhone = int(data[3])
-            duration = int(data[4])
+			# sólo se pasa a entero, suponemos que tienen el tamaño esperado 4B y 2B
+			callerPhone = int(data[2])
+			reciverPhone = int(data[3])
+			duration = int(data[4])
 
-            # empaquetamos los datos en el orden esperado
-            packedData = struct.pack('!cciih', hour, minute, callerPhone, reciverPhone, duration)
+			# empaquetamos los datos en el orden esperado
+			packedData = struct.pack("!cciih", hour, minute, callerPhone, reciverPhone, duration)
 
-            # Escribimos los datos empaquetados en el archivo
-            o.write(packedData)
-            print(f"Succesfully processed line: {line}", end = "")
+			# Escribimos los datos empaquetados en el archivo
+			outputFile.write(packedData)
+			print(f"Succesfully processed line: {line}", end = "")
 
 except FileNotFoundError as fnfe:
-    print("The file does not exit")
+	print(f"The \"{PATH_TO_INPUT_FILE}\" file does not exit.")
 except KeyboardInterrupt as ki:
-    print("The process has been killed")
+	print(f"The process has been killed.")
+except IndexError as ie:
+	print(f"Error trying to obtain a field from the file \"{PATH_TO_INPUT_FILE}\".")
 
